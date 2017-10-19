8062125fc6ad555ea1c5edfcca9e3c9d68c4ad66
Peter Maryniak <Pmaryniak9101@gmail.com>
2017-10-18 21:16:21 -0400

    Edited Chunky Monkey code to make it more efficient. Added Slasher FLcik File.

--- Chunky_Monkey.js
+++ Chunky_Monkey.js
@@ -1,10 +1,8 @@
 //This function splits an array into groups of length given in size. The result is returned in a two-dimensional array..
 function chunkArrayInGroups(arr, size) {
     var chunk = [];
-    var j = 0;
     for(var i = 0; i < arr.length; i += size) {
-        chunk[j] = arr.slice(i, i + size);;
-        j++;
+        chunk.push(arr.slice(i, i + size));
     }
 
     return chunk;
