94de540ef6d7f27f199015cdfcf0c2f6169117e8
Peter Maryniak <Pmaryniak9101@gmail.com>
2017-10-19 21:20:44 -0400

    Rearranged files o include Javascript exercises from other sources

--- Slasher_Flick.js
+++
@@ -1,6 +0,0 @@
-//Return the remaining elements of an array after chopping off "n" elements from the head.
-function slasher(arr, howMany) {
-    return arr.splice(howMany);
-}
-
-slasher(["burgers", "fries", "shake"], 1);
