9ee369beac73526bc7db54e50d7b65e196df086f
Peter Maryniak <Pmaryniak9101@gmail.com>
2017-10-29 21:59:07 -0400

    Edited Falsy Bouncer and edited code to be less redundant.

--- Freecodecamp/Basic Algorithms/Falsy Bouncer.js
+++ Freecodecamp/Basic Algorithms/Falsy Bouncer.js
@@ -1,7 +1,6 @@
 //Remove Falsy values in an array.
 function bouncer(arr) {
-    var list = arr.filter(Boolean);
-    return list;
+    return arr.filter(Boolean);
 }
 
 bouncer([false, null, 0, NaN, undefined, ""]);
