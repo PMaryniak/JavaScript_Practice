1eeed2ff19ef2b25baa36e938a5f1ec396ee5e80
Peter Maryniak <Pmaryniak9101@gmail.com>
2017-10-30 19:44:06 -0400

    Got rid of comment on Where Do I Belong.

--- Freecodecamp/Basic Algorithms/Where_Do_I_Belong.js
+++ Freecodecamp/Basic Algorithms/Where_Do_I_Belong.js
@@ -1,6 +1,5 @@
 //Place the value given in num into the array, sort it from low to high, and return the index at which the num appears in the array.
 function getIndexToIns(arr, num) {
-    // Find my place in this sorted array.
     arr.push(num);
     arr.sort(function(a, b) {
         return a - b;
